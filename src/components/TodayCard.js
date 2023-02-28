7aae9d3bc144bbc02a746cf6ef81471d3ee2486b
Fatih Makinist <f.mknst@gmail.com>
2023-02-28 16:08:32 +0300

    last version

--- src/components/TodayCard.js
+++ src/components/TodayCard.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import '../components/Today.css'
 
-function TodayCard({icon,descr,percentage,signal,number}) {
+function TodayCard({icon,descr,percentage,signal,number,textColor ,theme}) {
   return (
-    <div className='followers col-lg-2 col-sm-10  mt-4 me-5 ms-4'>
+    <div className={`followers col-lg-2 col-sm-10  mt-4 me-5 ms-4 ${theme} `}>
         <div className='d-flex justify-content-around mt-5'>
              <h4 className='text-muted'>{descr}</h4>
              {icon}
@@ -12,7 +12,7 @@ function TodayCard({icon,descr,percentage,signal,number}) {
             <strong className='fs-2 text-center'>{number}</strong>
             <div className='d-flex justify-content-between mt-3'>
             {signal}
-             <p className='mb-5 percentage'>{percentage}%</p>
+             <p className='mb-5 percentage' style={{color:textColor}}>{percentage}%</p>
             </div>
         </div>
   
--- src/components/TotalFollow.js
+++ src/components/TotalFollow.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import '../components/TotalFollowers.css'
 
-function TotalFollow({follow,userName,icon,signal,numberToday,textColor,border}) {
+function TotalFollow({follow,userName,icon,signal,numberToday,textColor,border,theme}) {
   return (
-    <div className='followers col-lg-2 col-sm-10  mt-2 me-5 ms-4' style={{borderTop: border}}>
+    <div className={`followers col-lg-2 col-sm-10  mt-2 me-5 ms-4  ${theme}`} style={{borderTop: border}}>
      <div className='d-flex mt-3  justify-content-center'>
      {icon}
      <p className='userName ms-2'>{userName}</p>
